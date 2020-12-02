27bc32c387d2000e198b33a7d5b14bff9fa943e5
Miguel <mabailon@espol.edu.ec>
2020-12-02 00:17:04 -0500

    Añadido Splash_Screen, Activity y XML menu_camas e instancia de función "iniciarSesion()".

---
+++ app/src/main/java/com/example/getabed/Splash_Screen.java
@@ -0,0 +1,26 @@
+package com.example.getabed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Screen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
--- app/src/main/java/com/example/getabed/menu_camas.java
+++ app/src/main/java/com/example/getabed/menu_camas.java
@@ -4,15 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class menu_camas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void iniciarSesion(){
-        
+        setContentView(R.layout.activity_menu_camas);
     }
 }
