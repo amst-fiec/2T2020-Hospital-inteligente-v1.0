3ec2006a60b001fab019e6b0627e79b51c517a5a
David Mendoza <drmendoz@espol.edu.ec>
2020-12-02 21:44:42 -0500

    Todas las activitys creadas

--- app/src/main/java/com/example/getabed/EstadoCamas.java
+++ app/src/main/java/com/example/getabed/EstadoCamas.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class menu_camas extends AppCompatActivity {
+public class EstadoCamas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_camas);
+        setContentView(R.layout.activity_estado_camas);
     }
 }
