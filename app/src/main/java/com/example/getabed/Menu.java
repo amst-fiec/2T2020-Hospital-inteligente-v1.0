c22df0a5b2ae36abf58c616d161c7626b7073473
Maria Eugenia Carrillo <maeucarr@espol.edu.ec>
2021-01-03 16:21:58 -0500

    coreccion

--- app/src/main/java/com/example/getabed/Menu.java
+++ app/src/main/java/com/example/getabed/Menu.java
@@ -13,6 +13,7 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        cerrarSesion();
     }
 
     public void verCamasDisponibles(View view){
