c08d321e3a9f16dfe48867fb239e5475e23e3508
Maria Eugenia Carrillo <maeucarr@espol.edu.ec>
2021-01-03 13:23:46 -0500

    cierre sesion

--- app/src/main/java/com/example/getabed/Menu.java
+++ app/src/main/java/com/example/getabed/Menu.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Menu extends AppCompatActivity {
@@ -19,8 +20,26 @@ public class Menu extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void cerrarSesion(View view){
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.button_cerrarsesion) {
+            cerrarSesion();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void cerrarSesion(){
         Intent intent= new Intent(getBaseContext(),MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 }
