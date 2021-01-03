7f90a96ca37a61f69740bafae00a787e2e93447f
Maria Eugenia Carrillo <maeucarr@espol.edu.ec>
2021-01-03 16:34:59 -0500

    cambios

--- app/src/main/java/com/example/getabed/Menu.java
+++ app/src/main/java/com/example/getabed/Menu.java
@@ -6,14 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Menu extends AppCompatActivity {
+    private Button bsignOut;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        cerrarSesion();
+        mAuth= FirebaseAuth.getInstance();
+        bsignOut = (Button) findViewById(R.id.button_cerrarsesion);
+        bsignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(Menu.this,MainActivity.class));
+                finish();
+            }
+        });
+        //cerrarSesion();
     }
 
     public void verCamasDisponibles(View view){
@@ -30,17 +48,17 @@ public class Menu extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.button_cerrarsesion) {
-            cerrarSesion();
-            return true;
-        }
+        //int id = item.getItemId();
+        //if (id == R.id.button_cerrarsesion) {
+            //cerrarSesion();
+            //return true;
+        //}
         return super.onOptionsItemSelected(item);
     }
 
     private void cerrarSesion(){
         Intent intent= new Intent(getBaseContext(),MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 }
