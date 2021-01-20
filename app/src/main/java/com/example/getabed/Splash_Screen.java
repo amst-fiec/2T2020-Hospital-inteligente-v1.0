5140316a66be273c64fc4b11f2e80ef3bcd15a89
David Mendoza <drmendoz@espol.edu.ec>
2021-01-20 18:02:38 -0500

    Inicio de sesion automatico

--- app/src/main/java/com/example/getabed/Splash_Screen.java
+++ app/src/main/java/com/example/getabed/Splash_Screen.java
@@ -7,7 +7,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Splash_Screen extends AppCompatActivity {
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,26 @@ public class Splash_Screen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash_Screen.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
+                auth=FirebaseAuth.getInstance();
+                FirebaseUser user=auth.getCurrentUser();
+
+                if(user!=null){
+                    Intent intent = new Intent(Splash_Screen.this,Menu.class);
+                    intent.putExtra("nombre",user.getDisplayName());
+                    intent.putExtra("imagen",String.valueOf(user.getPhotoUrl()));
+                    intent.putExtra("id",user.getUid());
+                    startActivity(intent);
+                    finish();
+
+                }else{
+                    Intent intent = new Intent(Splash_Screen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+           }
         },3000);
+
 
     }
 }
